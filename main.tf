ed8e7027e06dc79e212c906f56e6cd9455f9c62b
Rahil <rahilk82@gmail.com>
2023-03-26 01:10:25 +0530

    Added map of object examples fpr fpr_each loop function

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "main" {
   for_each   = var.vpc_cidr
-  cidr_block = each.value
+  cidr_block = each.value.cidr
+  instance_tenancy = each.value.instance_tenancy
 
-  tags = {
-    "Name" = each.key
-  }
+  tags = each.value["tags"]
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,24 @@
 variable "vpc_cidr" {
+  type = map(object({
+    cidr = string
+    instance_tenancy = string
+    tags = map(string)
+  }))
   default = {
-    "dev"  = "10.0.0.0/16",
-    "prod" = "10.0.0.0/24"
+    "1" = {
+      cidr = "10.0.0.0/16"
+      instance_tenancy = "default"
+      tags = {
+        "Name" = "dev"
+      }
+    }
+
+    "2" = {
+      cidr = "10.0.0.0/24"
+      instance_tenancy = "default"
+      tags = {
+        "Name" = "prod"
+      }
+    }
   }
 }
